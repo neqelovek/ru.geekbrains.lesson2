b9ca6dede518e523d03a979b7693f4640b41bc34
neqelovek <88616154+neqelovek@users.noreply.github.com>
2021-08-09 21:03:43 +0300

    Homework2

---
+++ src/lesson2.java
@@ -0,0 +1,20 @@
+public class lesson2 {
+
+    public static void main(String[] args){
+        doTask1();
+
+
+    }
+    static void doTask1(){
+        int[] mass = new int[8];
+        mass[0] = 0;mass[1] = 1;mass[2] = 1;mass[3] = 0;
+        mass[4] = 0;mass[5] = 1;mass[6] = 0;mass[7] = 1;
+        for (int a = 0; a < 8; a++){
+
+
+            System.out.println(mass[a]);
+        }
+}
+
+
+}
